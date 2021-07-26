988d068fb1ecadd7a1e0bd5233379b410b056e28
ayushthakur24 <at382377@gmail.com>
2021-07-26 01:26:12 -0400

    Second commit

--- src/main/java/com/spring/RestAPI/SpringRESTAPI/Controller/Controller.java
+++ src/main/java/com/spring/RestAPI/SpringRESTAPI/Controller/Controller.java
@@ -3,9 +3,13 @@ package com.spring.RestAPI.SpringRESTAPI.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +25,7 @@ public class Controller {
 	
 	@GetMapping("/courses")
 	public List<Course> getCourses(){
-		return courseService.getCourses();
+		return this.courseService.getCourses();
 	}
 	
 	@GetMapping("/courses/{courseId}")
@@ -33,4 +37,20 @@ public class Controller {
 	public Course addCourse(@RequestBody Course course) {
 		return this.courseService.addCourse(course);
 	}
-}
+	
+	@PutMapping("/courses")
+	public Course updateCourse(@RequestBody Course course) {
+		return this.courseService.updateCourse(course);
+	}
+	
+	@DeleteMapping("/courses/{courseId}")
+	public ResponseEntity<HttpStatus> deleteCourse(@PathVariable String courseId){
+		try {
+			this.courseService.deleteCourse(Long.parseLong(courseId));
+			return new ResponseEntity<>(HttpStatus.OK);
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		}
+	}
--- src/main/java/com/spring/RestAPI/SpringRESTAPI/Services/CourseServiceImpl.java
+++ src/main/java/com/spring/RestAPI/SpringRESTAPI/Services/CourseServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.RestAPI.SpringRESTAPI.Services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,11 @@ public class CourseServiceImpl implements CourseService {
 	
 	public CourseServiceImpl() {
 		list = new ArrayList<>();
-		list.add(new Course(124, "Java fundamentals", "This course is based on Java Programming"));
-		list.add(new Course(126,"Python Fundamentals","This course is based on Python Programming"));
+		list.add(new Course(111, "Java fundamentals", "This course is based on Java Programming"));
+		list.add(new Course(112,"Python Fundamentals","This course is based on Python Programming"));
+		list.add(new Course(113, "C++ fundamentals", "This course is based on C++ Programming"));
+		list.add(new Course(114, "C fundamentals", "This course is based on C Programming"));
+
 	}
 	@Override
 	public List<Course> getCourses() {
@@ -39,6 +43,21 @@ public class CourseServiceImpl implements CourseService {
 		list.add(course);
 		
 		return course;
+	}
+	@Override
+	public Course updateCourse(Course course) {
+		list.forEach(e -> {
+			if(e.getId() == course.getId()) {
+				e.setTitle(course.getTitle());
+				e.setDescription(course.getDescription());
+			}
+		});
+		return course;
+	}
+	@Override
+	public Course deleteCourse(long parseLong) {
+		list = this.list.stream().filter(e -> e.getId()!=parseLong).collect(Collectors.toList());
+		return null;
 	}
 	
 	
